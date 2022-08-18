78608d23d0cded4688091d5631b27129de1186f2
damian sola <damiansola99@gmail.com>
2022-08-18 17:13:26 -0300

    cambie algo nuevo

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const port = process.env.PORT || 3001
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-  server.listen(port, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+  server.listen(process.env.PORT, () => {
+    console.log("%s listening at 3000"); // eslint-disable-line no-console
   });
 });
