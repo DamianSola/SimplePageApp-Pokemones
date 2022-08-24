6a9e96e0ded1bc00fd3d31e8c20f0e85cf2a5e0a
damian sola <damiansola99@gmail.com>
2022-08-24 12:14:40 -0300

    cors conf

--- index.js
+++ index.js
@@ -19,13 +19,11 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
-const cors = require('cors')
 const port = process.env.PORT || 3001
 
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-  server.use(cors())
   server.listen(process.env.PORT, () => {
     console.log("%s listening at 3000"); // eslint-disable-line no-console
   });
