879b842a19dbde9a448191f8978c36ff493198ba
damian sola <damiansola99@gmail.com>
2022-08-25 17:19:43 -0300

    aaa

--- src/app.js
+++ src/app.js
@@ -4,6 +4,9 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const routes = require('./routes/index.js');
 const cors = require('cors')
+const {
+  URL_FRONT
+} = process.env;
 
 require('./db.js');
 
@@ -18,7 +21,7 @@ server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://simple-page-app-pokemones.vercel.app/'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin',URL_FRONT); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
