4f8183f61c25086870f583bdbba3c260664c013b
damian sola <damiansola99@gmail.com>
2023-05-17 15:27:57 -0300

    cambio de api

--- src/Redux/actions.js
+++ src/Redux/actions.js
@@ -1,9 +1,9 @@
 import axios from "axios";
-
+const url = "https://pokemonapi-i0tn.onrender.com/"
 
 const getAllPokemons = () => {
     return (dispatch) => {
-        return axios("https://pokemon-damian.herokuapp.com/pokemon")
+        return axios(url+"pokemon")
         .then((res) => {
             dispatch({
                 type: "GET_ALL_POKEMON",
@@ -15,7 +15,7 @@ const getAllPokemons = () => {
 
 const getPokeById = (id) => {
     return (dispatch) => {
-        return axios(`https://pokemon-damian.herokuapp.com/pokemon/${id}`)
+        return axios(`${url}pokemon/${id}`)
         .then((res) => {
             dispatch({
                 type: "GET_POKE_BY_ID",
@@ -27,7 +27,7 @@ const getPokeById = (id) => {
 
 const getPokeByName = (name) =>{
     return (dispatch) => {
-        return axios.get(`https://pokemon-damian.herokuapp.com/pokemon?name=${name}`)
+        return axios.get(`${url}pokemon?name=${name}`)
         .then((res) => {
             dispatch({
                 type: "GET_POKEMON_BY_NAME",
@@ -39,7 +39,7 @@ const getPokeByName = (name) =>{
 
 const getTipos  = () => {
     return (dispatch) => {
-        return axios.get("https://pokemon-damian.herokuapp.com/type")
+        return axios.get(url+"type")
         .then((res => {
             dispatch({
                 type: "GET_TYPES",
@@ -63,7 +63,7 @@ const filterNewPoke = () => {
 
 const createPokemon = (info) => {
     return () => {
-        axios.post("https://pokemon-damian.herokuapp.com/pokemons",info)
+        axios.post(url+"pokemons",info)
     }
 } 
 
